2d2246e94158878066627e1f58563af1ccecc76b
ZigZag-CS <ps96068@yahoo.com>
2020-04-15 19:30:37 +0300

    NEW - FINAL Kurs8 - adaugat pytils + metoda save

--- apps/scontent/models.py
+++ apps/scontent/models.py
@@ -1,15 +1,23 @@
+import datetime
 from django.conf import settings
 from django.db import models
-#from django.utils.text import slugify
-from pytils.translit import slugify
+from django.utils.text import slugify
+
+
+# from pytils.translit import slugify
+
+def image_directory_path(instance, filename):
+    time = str(datetime.date.today())
+    return 'users/{0}/{1}/{2}'.format(instance.user.username, time, filename)
 
 
 class Content(models.Model):
-    # on_delete=models.CASCADE/   https://stackoverflow.com/questions/38388423/what-does-on-delete-do-on-django-models
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='content_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=180, unique=True, verbose_name='Введите загаловок')
     slug = models.SlugField(max_length=220, db_index=True, verbose_name='Slug')
-    image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True, default='no_image_app_content.png',
+    # image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True,
+    # default='no_image_app_content.png', verbose_name='Загрузить изображение')
+    image = models.ImageField(upload_to=image_directory_path, blank=True, null=True, default='no_image_app_content.png',
                               verbose_name='Загрузить изображение')
     entry = models.TextField(blank=True, verbose_name='Запись')
     created = models.DateTimeField(auto_now_add=True)
