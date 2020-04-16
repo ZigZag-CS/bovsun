b83b9e25b50f6503d9d30c9cefc6d6ed409bcad6
ZigZag-CS <ps96068@yahoo.com>
2020-04-16 10:55:35 +0300

    FINAL Kurs9 - adaugat scontent app in adminka

--- apps/scontent/admin.py
+++ apps/scontent/admin.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from apps.scontent.models import Content
 
-# Register your models here.
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Content, ContentAdmin)
--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.index.urls', namespace='home_page')),
-    path('account/', include('apps.account.urls', namespace='account')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('apps.index.urls', namespace='home_page')),
+                  path('account/', include('apps.account.urls', namespace='account')),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
