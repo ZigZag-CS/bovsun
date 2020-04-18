e28be06c60328a120222ca97dc553387df197ac7
zigZag <ps96068@yahoo.com>
2020-04-18 17:14:03 +0300

    FINAL Kurs16 - s-a lucrat cu scontent2; s-a adaugat "def delete_content" in views.py, s-a adaugat in urls.py ".../delete/", s-a adaugat in detail.html link pu stergere, s-a adaugat delete.html pagina de confirmarea a shtergerii.

--- apps/scontent2/urls.py
+++ apps/scontent2/urls.py
@@ -6,8 +6,8 @@ app_name = 'scontent2'
 
 urlpatterns = [
     path('create/', views.content_create2, name='create_scontent2'),
-
     path('detail/<int:id>/<slug:slug>/', views.content_detail2, name='detail2'),
-    # url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.content_detail, name='detail'),
+    # url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.content_detail2, name='detail2'),
     path('detail/<int:id>/<slug:slug>/edit/', views.content_edit2, name='edit_detail2'),
+    path('detail/<int:id>/<slug:slug>/delete/', views.delete_content2, name='delete_content2'),
 ]
--- apps/scontent2/views.py
+++ apps/scontent2/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 
 from apps.scontent2.forms import *
@@ -42,3 +44,26 @@ def content_edit2(request, id, slug):
         form = ContentCreateForm2(instance=new_content)
     context = {'form': form, 'create_scontent2': False}
     return render(request, 'scontent2/create.html', context)
+
+
+@login_required
+def delete_content2(request, id, slug):
+    new_to_delete = get_object_or_404(Content2, id=id, slug=slug)
+
+    if new_to_delete.user != request.user and not request.user.is_superuser:
+        messages.error(request, "Эту запись вы не создавали, не можете удалить.")
+        return redirect(new_to_delete.get_absolute_url())
+
+    if request.method == 'POST':
+        form = DeleteContentForms2(request.POST, instance=new_to_delete)
+
+        if form.is_valid():
+            new_to_delete.delete()
+            messages.success(request, 'Запись успешно удалена')
+            return HttpResponseRedirect('/')
+
+    else:
+        form = DeleteContentForms2(instance=new_to_delete)
+
+    tempate_vars = {'form': form}
+    return render(request, 'scontent2/delete.html', tempate_vars)
